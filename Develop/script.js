53c738d94ec7e5465beeba608fea278b1905b209
burcar <zacharyburcar@gmail.com>
2021-10-22 18:37:33 -0400

    final touches

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,20 @@
+// an alert/prompt box free password generator. Giving the user all possible
+// password options up front.
+
+// WHAT THIS CODE DOES
+
+// The user verfies what they would like their password contain,
+// this is ran through a check function that recognizes the character types chosen
+// these are called from global strings
+// the checks are called from their corresponding html id
+// the password is created from a for loop that chooses a random character from the
+// generated 'full' character string.
+// the length varaiable references the desired password length and is used in the function.
+// Finally two additionally checks are made before revealing the password
+// if no boxes are check, the password.text area is filled with an error message telling the user they must check
+// at least one box. if a number below 8 or above 128 is inputted in Length, a seperate error is pushed.
+
+
 // Get references html elements
 let generateBtn = document.querySelector("#generate");
 let refreshBtn = document.querySelector("#refresh");
@@ -16,8 +33,9 @@ let symbols = '!@#$%^&*?';
 let upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 let lowerCase = 'abcdefghijklmnopqrstuvwxyz';
 
+// checkBox function that checks which checkBoxes are checked!
 function checkBox() {
- 
+    // checks to see if the user has checked any of the 4 available check boxes
     getLower.checked ? (full += lowerCase) : "" - lowerCase;
     getUpper.checked ? (full += upperCase) : "" - upperCase;
     getNumbers.checked ? (full += numbers) : "" - numbers,
@@ -26,8 +44,6 @@ function checkBox() {
       console.log(full);
       return full;
 }
-
-
 
 // Write password to the #password input
 function writePassword() {
@@ -40,10 +56,12 @@ function writePassword() {
   passwordText.value = generatePassword(length.value, characters);
 
   // checking to see if user is attempting to generate a password that is too small or too large
-  if (length.value < 8 || length.value > 128) {
-    passwordText.value = "You need at least one character type checked to generate a password!"
+  if (length < 8 || length > 128) {
+    // push new string reminding user to input a valid number option
+    passwordText.value = "You need at least one character type checked to generate a password!";
   }
   else {
+    // reveal generated password
     passwordText.value = password;
   }
   // checking to see if all checkboxes are unchecked
@@ -51,9 +69,11 @@ function writePassword() {
     && !getUpper.checked
     && !getNumbers.checked
     && !getSymbols.checked) {
-    passwordText.value = "You need at least one character type checked to generate a password!"
+      // push new string to take html placeholder text's place 
+    passwordText.value = "You need at least one character type checked to generate a password!";
     }
     else {
+      // reveal generated password.
       passwordText.value = password;
     }
 
@@ -67,6 +87,7 @@ let generatePassword = (length, characters) => {
   for (let i = 0; i < length; i++) {
     // for loop calls the characters parameter
     password += characters.charAt(
+      // Math.random to randomize a string of approved characters of the user's desired length.
       Math.floor(Math.random() * characters.length)
       );
   }
@@ -75,6 +96,7 @@ let generatePassword = (length, characters) => {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// Event listener to refresh button
 refreshBtn.addEventListener("click", refreshPage);
 
 function refreshPage() {
